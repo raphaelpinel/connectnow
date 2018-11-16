34190d6d63a962a2a1ce7abe4c5bc3c4667b65cf
Raphael Pinel <raphaelpinel8@gmail.com>
2018-11-16 11:58:42 +0200

    basckend for users login

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,30 +1,30 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const gravatar = require("gravatar");
-const bcrypt = require("bcryptjs");
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
 // Load User model
-const User = require("../../models/User");
+const User = require('../../models/User');
 
 // @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
-router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
+router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
 
 // @route GET api/users/register
 // @desc Register user
 // @access Public
-router.post("/register", (req, res) => {
+router.post('/register', (req, res) => {
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
       return res.status(400).json({
-        email: "Email already exists"
+        email: 'Email already exists'
       });
     } else {
       const avatar = gravatar.url(req.body.email, {
-        s: "200", // Size
-        r: "pg", // rating, "may contain rude gestures, provocatively dressed individuals, the lesser swear words, or mild violence." see API: https://en.gravatar.com/site/implement/images/
-        d: "mm" // default
+        s: '200', // Size
+        r: 'pg', // rating, "may contain rude gestures, provocatively dressed individuals, the lesser swear words, or mild violence." see API: https://en.gravatar.com/site/implement/images/
+        d: 'mm' // default
       });
       const newUser = new User({
         name: req.body.name,
@@ -46,4 +46,29 @@ router.post("/register", (req, res) => {
     }
   });
 });
+
+// @route   GET api/users/login
+// @desc    Login User / Returning Token
+// @access  Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    // Check for user
+    if (!user) {
+      return res.status(404).json({ email: 'User not found' });
+    }
+    // Check Password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: 'Success' });
+      } else {
+        return res.status(400).json({ password: 'Password incorrect' });
+      }
+    });
+  });
+});
+
 module.exports = router;
